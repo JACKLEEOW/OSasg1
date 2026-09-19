b702cb504cdb3a5689cf5c1b39497ee00d2bd561
Anthony Muma <amuma917@mtroyal.ca>
2026-09-19 01:10:31 -0600

    feat: added quote handling

--- Stage1/tokens.c
+++ Stage1/tokens.c
@@ -20,23 +20,41 @@ Tokens create_tokens(char * s) {
         return t;
     }
 
-    int i = 0;
-    //char currentQuote = '\0';
+    int i = 0; // iterator for input string
+    int j = 0; // iterator for copied string
+    char curr_quote = '\0';
     while (1) {
 
         // move to the right until non-ws character
-        while(c_isspace((unsigned char)s[i])) {t.token_strings[i] = s[i]; i++;}
+        while(c_isspace((unsigned char)s[i])) {t.token_strings[j] = s[i]; i++; j++;}
         if (s[i] == '\0') break;
+        if (is_quote(s[i])) {curr_quote = s[i]; i++;} // skip pass quote
 
         // set pointer to start of string
-        t.token_string_ptrs[t.num_tokens] = t.token_strings + i;
+        t.token_string_ptrs[t.num_tokens] = t.token_strings + j;
         t.num_tokens ++;
 
-        // move to the right until ws character
-        while(!c_isspace((unsigned char)s[i]) && s[i] != '\0') {t.token_strings[i] = s[i]; i++;}
-        if (s[i] == '\0') break; // if character was null terminator, end
-        t.token_strings[i] = '\0'; // otherwise, add a \0 separator
+        
+        // quote mode
+        if (curr_quote) {
+            QuoteMode:
+            // move to the right until closing quote character (read anything)
+            while(s[i] != curr_quote && s[i] != '\0') {t.token_strings[j] = s[i]; i++; j++;}
+            if (s[i] == '\0') {t.token_strings[j] = '\0'; break;} // maybe set a flag
+            curr_quote = '\0'; // reset quote
+            i++;
+        }
+            
+        // move to the right until ws character, quote, or operator
+        while(!c_isspace((unsigned char)s[i]) && s[i] != '\0' && !is_quote(s[i])) {t.token_strings[j] = s[i]; i++; j++;}
+        if (s[i] == '\0') {t.token_strings[j] = '\0'; break;} // if character was null terminator, end
+
+        // found in between (operators)
+        if (is_quote(s[i])) {curr_quote = s[i]; i++; goto QuoteMode;} // if quote found, skip pass string pointer, l"s" -> ls, cstring pointer to l from previous
+
+        t.token_strings[j] = '\0'; // otherwise, add a \0 separator
         i++;
+        j++;
     }
     return t;
 }
@@ -54,7 +72,8 @@ void test_tokens(Tokens * t) {
     printf("Number of Tokens: %d\n", t->num_tokens);
     int i;
     for (i = 0; i < num_tokens(t); i++) {
-        printf("Token: %s\n", index_tokens(t, i));
+        char * s = index_tokens(t, i);
+        printf("Token: %s (%ld)\n", s, c_strlen(s));
     }
 }
 
