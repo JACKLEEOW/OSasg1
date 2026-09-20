361f00d6e5cff2f779478481ada4e230e407c236
Jack L <jack.le.031231@hotmail.com>
2026-09-20 15:42:04 -0600

    refactored loop

--- JackAttempt/test.c
+++ JackAttempt/test.c
@@ -1,31 +1,37 @@
 #include "cstring.h"
-#include <unistd.h>
 #include "stdio.h"
+#include <unistd.h>
 #define BUFFER_SIZE 128
-#define DOLLAR_SIGN "$"
+#define DOLLAR_SIGN "$ "
+#define EXIT "exit"
+#define NEWLINE '\n'
 int main() {
 
   while (1) {
+    write(STDOUT_FILENO, DOLLAR_SIGN, 2);
     char buffer[BUFFER_SIZE];
 
-    char str[] = "hello12323";
-    char str2[] = "hello12323";
-
     ssize_t n = read(STDIN_FILENO, buffer, BUFFER_SIZE);
-    if (n > 0) {
-      write(STDOUT_FILENO, buffer, n);
+    if (n <= 0) {
+      break;
     }
 
-    if(c_stringcmp(str,str2) == 1) {
-        printf("MATCH\n");
-
+    if (n < BUFFER_SIZE) {
+      buffer[n] = '\0'; // null terminate buffer
     }
-    else {
-        printf("NO MATCH\n");
+    if (buffer[n - 1] == '\n') { // if user presses enter, change to null term
+      buffer[n - 1] = '\0';
     }
 
- 
+    if (buffer[0] == '\0') {
+      continue; // empty line case user types enter
+    }
+
+    if (c_stringcmp(buffer, EXIT) == 1) {
+      return 0;
+    }
+    write(STDOUT_FILENO,buffer,n);
+    write(STDOUT_FILENO,"\n", 1);
   }
-
   return 0;
 }
